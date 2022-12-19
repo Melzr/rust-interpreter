95a93f42d70eb0df0246356b99b4af10904381f5
Melina Lazzaro <melinalazzaro01@gmail.com>
2022-12-19 19:42:13 -0300

    update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject rust-interpreter "0.1.0-SNAPSHOT"
-  :description "Rust interpreter in Clojure and runs on the JVM"
-  :url "http://example.com/FIXME"
+  :description "Rust interpreter that runs on the JVM"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
